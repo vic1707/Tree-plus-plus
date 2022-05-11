0c3c78f9b8d0610f59c1309b46977cbefdb09192
vic1707 <28602203+vic1707@users.noreply.github.com>
2022-05-11 16:22:26 +0200

    fix indentation

--- src/args.cpp
+++ src/args.cpp
@@ -1,11 +1,11 @@
 #include "args.hpp"
 
 bool cmdOptionExists(char** begin, char** end, const char** &option) {
-    return std::find(begin, end, option) != end;
+  return std::find(begin, end, option) != end;
 }
 
 char* getCmdOption(char** begin, char** end, const char** &option) {
-    char** itr = std::find(begin, end, option);
-    if (itr != end && ++itr != end) return *itr;
-    return 0;
+  char** itr = std::find(begin, end, option);
+  if (itr != end && ++itr != end) return *itr;
+  return 0;
 }
