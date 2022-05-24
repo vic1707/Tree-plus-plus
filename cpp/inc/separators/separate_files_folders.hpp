1810557c18a9c72e0cf867e9de0ab545f606eba0
vic1707 <28602203+vic1707@users.noreply.github.com>
2022-05-24 10:27:58 +0200

    separator doesn't have to sort

--- cpp/inc/separators/separate_files_folders.hpp
+++ cpp/inc/separators/separate_files_folders.hpp
@@ -6,20 +6,12 @@
   namespace Separator {
     template<class First, class Second>
     class FileFolder : public ASeparator {
-      private:
-        template <class T>
-        auto compare() {
-          return [](const auto& a, const auto& b) { return std::get<T>(a).path < std::get<T>(b).path; };
-        }
-
       public:
         void separate(Items &items) final override {
-          auto middle_part = std::partition(items.begin(), items.end(), [](const auto &item) {
+          std::partition(items.begin(), items.end(), [](const auto &item) {
             return std::holds_alternative<First>(item);
           });
-          std::sort(items.begin(), middle_part, compare<First>());
-          std::sort(middle_part, items.end(), compare<Second>());
-        }
+        };
     };
   } // namespace Separator
 #endif // FILE_FOLDER_SEPARATOR_HPP
