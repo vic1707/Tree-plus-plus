868c02ab84d4c4bae77ebc45dd2c4de5561f0445
vic1707 <28602203+vic1707@users.noreply.github.com>
2022-05-28 12:45:13 +0200

    need to rethink counter

--- cpp/src/file_dir_infos.cpp
+++ cpp/src/file_dir_infos.cpp
@@ -4,14 +4,6 @@ namespace FileDirInfos {
   template <typename Item>
   inline void DirInfos::push_item(const fs::directory_entry &entry) noexcept {
     Item item(entry);
-    if constexpr (std::is_same_v<Item, DirInfos>) {
-      ++child_count.dirs;
-      total_child_count.dirs += item.child_count.dirs + 1;
-      total_child_count.files += item.total_child_count.files;
-    } else {
-      ++child_count.files;
-      ++total_child_count.files;
-    }
     size += item.size.bytes;
     items.emplace_back(std::move(item));
   }
