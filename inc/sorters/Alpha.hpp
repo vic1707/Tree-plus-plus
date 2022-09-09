8065945599b78e86d0978a25c9577c1309a8bc99
vic1707 <28602203+vic1707@users.noreply.github.com>
2022-09-09 22:03:33 +0200

    fix alpha sort case

--- inc/sorters/Alpha.hpp
+++ inc/sorters/Alpha.hpp
@@ -1,23 +1,35 @@
 #pragma once
 /* std */
+#include <algorithm>
+#include <cctype>
 #include <string>
+#include <iostream>
 /* custom */
 #include <displayers_specs/Sorter.hpp>
+
+inline void char_to_lower(char& c) { c = std::tolower(c); };
+inline void word_to_lower(std::string& w) {
+  std::for_each(w.begin(), w.end(), char_to_lower);
+}
 
 namespace Sorter {
   class Alpha : public ASorter {
     public:
       void sort(Items &items) final {
         std::stable_sort(items.begin(), items.end(), [this](const auto &a, const auto &b) {
-          return this->get_path(a) < this->get_path(b);
+          std::string a_path = this->get_filename(a);
+          std::string b_path = this->get_filename(b);
+          word_to_lower(a_path);
+          word_to_lower(b_path);
+          return a_path < b_path;
         });
       }
     private:
-      fs::path get_path(const auto &i) {
+      fs::path get_filename(const auto &i) {
         if (std::holds_alternative<FileDirInfos::DirInfos>(i))
-          return std::get<FileDirInfos::DirInfos>(i).path;
+          return std::get<FileDirInfos::DirInfos>(i).path.filename();
         else
-          return std::get<FileDirInfos::FileInfos>(i).path;
+          return std::get<FileDirInfos::FileInfos>(i).path.filename();
       };
   };
 } // namespace Sorter
