3b32000fc371b0369ceb43f6886cbdb90699b42d
vic1707 <28602203+vic1707@users.noreply.github.com>
2022-09-09 21:31:01 +0200

    sorters: move specific getters to private methods

--- inc/sorters/Alpha.hpp
+++ inc/sorters/Alpha.hpp
@@ -8,15 +8,16 @@ namespace Sorter {
   class Alpha : public ASorter {
     public:
       void sort(Items &items) final {
-        std::stable_sort(items.begin(), items.end(), [](const auto &a, const auto &b) {
-          auto get_path = [](const auto &i) -> const auto& {
-            if (std::holds_alternative<FileDirInfos::DirInfos>(i))
-              return std::get<FileDirInfos::DirInfos>(i).path;
-            else
-              return std::get<FileDirInfos::FileInfos>(i).path;
-          };
-          return get_path(a) < get_path(b);
+        std::stable_sort(items.begin(), items.end(), [this](const auto &a, const auto &b) {
+          return this->get_path(a) < this->get_path(b);
         });
       }
+    private:
+      fs::path get_path(const auto &i) {
+        if (std::holds_alternative<FileDirInfos::DirInfos>(i))
+          return std::get<FileDirInfos::DirInfos>(i).path;
+        else
+          return std::get<FileDirInfos::FileInfos>(i).path;
+      };
   };
 } // namespace Sorter
--- inc/sorters/Extension.hpp
+++ inc/sorters/Extension.hpp
@@ -8,15 +8,16 @@ namespace Sorter {
   class Extension : public ASorter {
     public:
       void sort(Items &items) final {
-        std::stable_sort(items.begin(), items.end(), [](const auto &a, const auto &b) {
-          auto get_extension = [](const auto &i) -> std::string {
-            if (std::holds_alternative<FileDirInfos::DirInfos>(i))
-              return std::get<FileDirInfos::DirInfos>(i).name.extension;
-            else
-              return std::get<FileDirInfos::FileInfos>(i).name.extension;
-          };
-          return get_extension(a) < get_extension(b);
+        std::stable_sort(items.begin(), items.end(), [this](const auto &a, const auto &b) {
+          return this->get_extension(a) < this->get_extension(b);
         });
       }
+    private:
+      std::string get_extension(const auto &i) {
+        if (std::holds_alternative<FileDirInfos::DirInfos>(i))
+          return std::get<FileDirInfos::DirInfos>(i).name.extension;
+        else
+          return std::get<FileDirInfos::FileInfos>(i).name.extension;
+      };
   };
 } // namespace Sorter
