699adbd231e47bf1465d7a3da68a4425d1c9b59e
vic1707 <28602203+vic1707@users.noreply.github.com>
2022-09-10 19:56:29 +0200

    try fix win CI by restauring

--- xmake.lua
+++ xmake.lua
@@ -4,11 +4,17 @@ add_rules("mode.release", "mode.debug")
 -- add requires libs
 add_requires("fmt")
 
--- set c++ standard
-set_languages("c++20")
+-- set arch
+set_allowedarchs("armv7-a", "arm64-v8a", "armv5te", "mips", "mips64", "i386", "x86_64")
 
-target("treepp")
-    set_kind("binary")
-    add_files("src/**.cpp")
-    add_includedirs("inc")
-    add_packages("fmt")
+archs = { "armv7-a", "arm64-v8a", "armv5te", "mips", "mips64", "i386", "x86_64" }
+-- add targets
+for _, arch in ipairs(archs) do
+    target("treepp_" .. os.host() .. "_" .. arch)
+        set_kind("binary")
+        set_arch(arch)
+        add_files("src/**.cpp")
+        add_includedirs("inc")
+        set_languages("c++20")
+        add_packages("fmt")
+end
