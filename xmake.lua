d93e21502aa9ffb7cd7807fb85d8f57c238da225
vic1707 <28602203+vic1707@users.noreply.github.com>
2022-09-10 18:29:18 +0200

    gg

--- xmake.lua
+++ xmake.lua
@@ -4,9 +4,11 @@ add_rules("mode.release", "mode.debug")
 -- add requires libs
 add_requires("fmt")
 
+-- set c++ standard
+set_languages("c++20")
+
 target("treepp")
     set_kind("binary")
     add_files("src/**.cpp")
     add_includedirs("inc")
-    set_languages("c++20")
     add_packages("fmt")
