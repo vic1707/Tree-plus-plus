d147dedbf836c62a93c4d811d16610ebcd55b67c
vic1707 <28602203+vic1707@users.noreply.github.com>
2022-09-11 17:27:06 +0200

    disable windows flags for now

--- xmake.lua
+++ xmake.lua
@@ -4,7 +4,6 @@ add_rules("mode.release", "mode.debug")
 -- add requires libs
 add_requires("fmt")
 
-
 target("treepp")
     set_kind("binary")
     set_languages("cxx20")
@@ -12,7 +11,7 @@ target("treepp")
     add_includedirs("inc")
     add_packages("fmt")
     if is_plat("windows") then
-        add_cxxflags("/Wall /WX")
+        -- add_cxxflags("/Wall /WX")
     else
         add_cxxflags("-Wall -Wextra -pedantic")
     end
