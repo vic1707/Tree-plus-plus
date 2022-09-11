c4a1ec0229c5bf5ba76ef54b4f273499c05feff7
vic1707 <28602203+vic1707@users.noreply.github.com>
2022-09-11 16:31:21 +0200

    move `set_language`

--- xmake.lua
+++ xmake.lua
@@ -4,11 +4,10 @@ add_rules("mode.release", "mode.debug")
 -- add requires libs
 add_requires("fmt")
 
--- set c++ standard
-set_languages("c++20")
 
 target("treepp")
     set_kind("binary")
+    set_languages("c++20")
     add_files("src/**.cpp")
     add_includedirs("inc")
     add_packages("fmt")
