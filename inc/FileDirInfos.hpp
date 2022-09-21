b0bfc76219d93ff45c90030c1c8ec51132e1b6ca
vic1707 <28602203+vic1707@users.noreply.github.com>
2022-09-21 08:40:00 +0200

    restaure better `<<` ChildCount

--- inc/FileDirInfos.hpp
+++ inc/FileDirInfos.hpp
@@ -52,9 +52,12 @@ namespace FileDirInfos {
     /* Operators */
     friend inline std::ostream &operator<<(std::ostream &os, const ChildCount &children) {
       if (children.total.dirs == 0 && children.total.files == 0) return os << "Empty directory";
-      if (children.total.dirs == 0) return os << children.total.files << " file" << (children.total.files > 1 ? "s" : "");
-      if (children.total.files == 0) return os << children.total.dirs << " director" << (children.total.dirs > 1 ? "ies" : "y");
-      return os << children.total.dirs << " director" << (children.total.dirs > 1 ? "ies" : "y") << " and " << children.total.files << " file" << (children.total.files > 1 ? "s" : "");
+      auto const dirs_plural = children.local.dirs < 1 ? "y" : "ies";
+      auto const files_plural = children.local.files < 1 ? "" : "s";
+      auto const total_dirs = children.local.dirs != children.total.dirs ? fmt::format(" ({} total)", children.total.dirs) : std::string();
+      auto const total_files = children.local.files != children.total.files ? fmt::format(" ({} total)", children.total.files) : std::string();
+      auto result = fmt::format("{} director{}{} and {} file{}{}", children.local.dirs, dirs_plural, total_dirs, children.local.files, files_plural, total_files);
+      return os << result;
     }
   };
 
