6dd199c193c4c940934b1d75180ed3762dbadb34
vic1707 <28602203+vic1707@users.noreply.github.com>
2022-09-30 11:56:17 +0200

    fix extension filter

--- inc/filters/Extensions.hpp
+++ inc/filters/Extensions.hpp
@@ -3,6 +3,7 @@
 #include <string>
 /* custom */
 #include <displayers_specs/Filter.hpp>
+#include <variant>
 
 namespace Filter {
   class Extensions : public AFilter {
@@ -12,17 +13,14 @@ namespace Filter {
       /* Methods */
       void filter(FileDirInfos::DirInfos &parent) final {
         for (auto it = parent.items.begin(); it != parent.items.end(); ++it) {
-          if (std::find(m_discriminants.begin(), m_discriminants.end(), this->get_extension(*it)) != m_discriminants.end()) return;
+          if (std::holds_alternative<FileDirInfos::DirInfos>(*it)) continue;
+          auto &ext = std::get<FileDirInfos::FileInfos>(*it).name.extension;
+          // if the extension isn't empty, remove the dot
+          ext = ext.empty() ? ext : ext.substr(1);
+          if (m_discriminants.find(ext) == m_discriminants.end()) continue;
           remove_from_parent(it, parent);
           --it;
         }
       }
-    private:
-      std::string get_extension(const auto &i) {
-        if (std::holds_alternative<FileDirInfos::DirInfos>(i))
-          return std::get<FileDirInfos::DirInfos>(i).name.extension;
-        else
-          return std::get<FileDirInfos::FileInfos>(i).name.extension;
-      };
   };
 } // namespace Filter
