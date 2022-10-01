0d8011f59e21b767a439b0f37f6582f98a5bf622
vic1707 <28602203+vic1707@users.noreply.github.com>
2022-10-01 18:19:04 +0200

    simplify options for POC

--- other-implementations/rust-tree-plus-plus/src/options.rs
+++ other-implementations/rust-tree-plus-plus/src/options.rs
@@ -4,35 +4,15 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 pub struct Args {
-    /// Boolean flag to enable listing hidden files
-    #[clap(short, long)]
-    pub all_files: bool,
+  /// Boolean flag to enable listing hidden files
+  #[clap(short, long)]
+  pub all_files: bool,
 
-    /// Boolean flag to show size in bytes
-    #[clap(long)]
-    pub size_in_bytes: bool,
+  /// Size of tab indentation
+  #[clap(long, default_value = "2")]
+  pub tab_size: usize,
 
-    /// Boolean flag to redirect to external file
-    #[clap(short, long)]
-    pub redirect: bool,
-
-    /// Size of tab indentation
-    #[clap(long, default_value = "2")]
-    pub tab_size: usize,
-
-    /// List of sorting options
-    #[clap(long)]
-    pub sort_by: Vec<String>,
-
-    /// Formatter to use
-    #[clap(long, default_value = "name_only")]
-    pub format: String,
-
-    /// Indenter to use
-    #[clap(long, default_value = "fancy")]
-    pub indenter: String,
-
-    /// Positional argument for the paths to list
-    #[clap(parse(from_os_str), default_value = ".")]
-    pub paths: Vec<std::path::PathBuf>,
+  /// Positional argument for the paths to list
+  #[clap(default_value = ".")]
+  pub paths: Vec<String>,
 }
