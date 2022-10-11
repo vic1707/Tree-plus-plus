1db66f0235d67e0465ab3d41de6e6afd7ae93f1e
vic1707 <28602203+vic1707@users.noreply.github.com>
2022-10-11 08:21:39 +0200

    `FileInfos` is similar to `DirInfos`

--- other-implementations/rust-tree-plus-plus/src/file_dir_infos/file_infos.rs
+++ other-implementations/rust-tree-plus-plus/src/file_dir_infos/file_infos.rs
@@ -1,11 +1,15 @@
 use crate::file_dir_infos::item_infos::ItemInfos;
 
 /// A struct that contains the information of a file.
-pub struct FileInfos(ItemInfos);
+pub struct FileInfos {
+  infos: ItemInfos
+}
 
 impl FileInfos {
   /// Creates a new `FileInfos` struct.
   pub fn new(path: &str) -> Self {
-    Self(ItemInfos::new(path))
+    Self {
+      infos: ItemInfos::new(path)
+    }
   }
 }
