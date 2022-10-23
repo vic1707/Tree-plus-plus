98587609ab98b25bd8acba3b706e3cf2193c1886
vic1707 <28602203+vic1707@users.noreply.github.com>
2022-10-23 19:02:29 +0200

    fix `AbbreviatedLowercase` names

--- inc/size/unit.hpp
+++ inc/size/unit.hpp
@@ -56,22 +56,22 @@ namespace size {
 
   constexpr const Unit BYTE = {Unit_Val::BYTE, {"B", "b", "byte", "byte"}};
   constexpr const std::array<const Unit, 16> units = {{
-    { Unit_Val::KIBIBYTE, {"KiB", "KiB",  "Kibibyte",  "kibibyte"}},
-    { Unit_Val::KILOBYTE, { "KB",  "kB",  "Kilobyte",  "kilobyte"}},
-    { Unit_Val::MEBIBYTE, {"MiB", "MiB",  "Mebibyte",  "mebibyte"}},
-    { Unit_Val::MEGABYTE, { "MB",  "MB",  "Megabyte",  "megabyte"}},
-    { Unit_Val::GIBIBYTE, {"GiB", "GiB",  "Gibibyte",  "gibibyte"}},
-    { Unit_Val::GIGABYTE, { "GB",  "GB",  "Gigabyte",  "gigabyte"}},
-    { Unit_Val::TEBIBYTE, {"TiB", "TiB",  "Tebibyte",  "tebibyte"}},
-    { Unit_Val::TERABYTE, { "TB",  "TB",  "Terabyte",  "terabyte"}},
-    { Unit_Val::PEBIBYTE, {"PiB", "PiB",  "Pebibyte",  "pebibyte"}},
-    { Unit_Val::PETABYTE, { "PB",  "PB",  "Petabyte",  "petabyte"}},
-    { Unit_Val::EXBIBYTE, {"EiB", "EiB",  "Exbibyte",  "exbibyte"}},
-    {  Unit_Val::EXABYTE, { "EB",  "EB",   "Exabyte",   "exabyte"}},
-    { Unit_Val::ZEBIBYTE, {"ZiB", "ZiB",  "Zebibyte",  "zebibyte"}},
-    {Unit_Val::ZETTABYTE, { "ZB",  "ZB", "Zettabyte", "zettabyte"}},
-    { Unit_Val::YOBIBYTE, {"YiB", "YiB",  "Yobibyte",  "yobibyte"}},
-    {Unit_Val::YOTTABYTE, { "YB",  "YB", "Yottabyte", "yottabyte"}}
+    { Unit_Val::KIBIBYTE, {"KiB", "kib",  "Kibibyte",  "kibibyte"}},
+    { Unit_Val::KILOBYTE, { "KB",  "kb",  "Kilobyte",  "kilobyte"}},
+    { Unit_Val::MEBIBYTE, {"MiB", "mib",  "Mebibyte",  "mebibyte"}},
+    { Unit_Val::MEGABYTE, { "MB",  "mb",  "Megabyte",  "megabyte"}},
+    { Unit_Val::GIBIBYTE, {"GiB", "gib",  "Gibibyte",  "gibibyte"}},
+    { Unit_Val::GIGABYTE, { "GB",  "gb",  "Gigabyte",  "gigabyte"}},
+    { Unit_Val::TEBIBYTE, {"TiB", "tib",  "Tebibyte",  "tebibyte"}},
+    { Unit_Val::TERABYTE, { "TB",  "tb",  "Terabyte",  "terabyte"}},
+    { Unit_Val::PEBIBYTE, {"PiB", "pib",  "Pebibyte",  "pebibyte"}},
+    { Unit_Val::PETABYTE, { "PB",  "pb",  "Petabyte",  "petabyte"}},
+    { Unit_Val::EXBIBYTE, {"EiB", "eib",  "Exbibyte",  "exbibyte"}},
+    {  Unit_Val::EXABYTE, { "EB",  "eb",   "Exabyte",   "exabyte"}},
+    { Unit_Val::ZEBIBYTE, {"ZiB", "zib",  "Zebibyte",  "zebibyte"}},
+    {Unit_Val::ZETTABYTE, { "ZB",  "zb", "Zettabyte", "zettabyte"}},
+    { Unit_Val::YOBIBYTE, {"YiB", "yib",  "Yobibyte",  "yobibyte"}},
+    {Unit_Val::YOTTABYTE, { "YB",  "yb", "Yottabyte", "yottabyte"}}
   }};
 
   constexpr const Unit& get_unit(const BT bytes, const int base) {
